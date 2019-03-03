e8df47728e63a77ca5d2523a690a62d155d868bc
nirvanarsc <nirvana_rsc@yahoo.com>
2019-03-03 15:00:49 +0900

    Create compare-the-triplets.js

---
+++ Warmup/compare-the-triplets.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let alice = 0;
+    let bob = 0;
+
+    let checkEach = function (z, y) {
+        if (z > y) {
+            alice += 1;
+        }
+        else if (z < y) {
+            bob += 1;
+        }
+    }
+
+    for (var i = 0; i < 3; i++) {
+        checkEach(a[i], b[i]);
+    }
+
+    return [alice, bob];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
