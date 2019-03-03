9043790e3695eb5f233136abc056c466630c44c4
Teo <nirvana_rsc@yahoo.com>
2019-03-03 15:59:04 +0900

    Staircase in C

---
+++ Warmup/staircase.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void staircase(int n)
+{
+    for (int x = 0; x < n; x++)
+    {
+        int spaces = n - (x + 1);
+        int stars = x + 1;
+        while (spaces > 0)
+        {
+            printf(" ");
+            spaces--;
+        }
+        while (stars)
+        {
+            printf("#");
+            stars--;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int i = 6;
+    staircase(i);
+}
