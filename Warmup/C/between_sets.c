dec444d6456130a42b896164c84d0a128fc23a45
Teo <nirvana_rsc@yahoo.com>
2019-04-03 15:47:49 +0900

    Finish Between Sets in C

--- Warmup/C/between_sets.c
+++ Warmup/C/between_sets.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include "convenient.h"
 
-int main() {
-  int a[] = {65, 10, 5};
-  int a_length = sizeof(a) / sizeof(a[0]);
+int countMultiples(int a, int b) {
+  int res = 0;
+  for (int i = a, j = 2; i <= b; i = a * j, j++) {
+    if (b % i == 0) {
+      res++;
+    }
+  }
+  return res;
+}
 
-  printf("%d\n", arrayApply(a, a_length, gcd));
-  printf("%d\n", arrayApply(a, a_length, lcm));
+int getTotalX(int a_count, int* a, int b_count, int* b) {
+  return countMultiples(arrayApply(a, a_count, lcm),
+                        arrayApply(b, b_count, gcd));
+}
+
+int main() {
+  int a[] = {2, 4};
+  int a_length = sizeof(a) / sizeof(a[0]);
+  int b[] = {16, 32, 96};
+  int b_length = sizeof(b) / sizeof(b[0]);
+
+  printf("%d\n", getTotalX(a_length, a, b_length, b));
 }
