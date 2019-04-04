01c2c7d5533c73c1a7685a920b878ca4f68baff7
nirvanarsc <nirvana_rsc@yahoo.com>
2019-04-04 19:49:09 +0900

    Sock Merchant WIP

---
+++ Warmup/C/sock_merchant.c
@@ -0,0 +1,14 @@
+
+
+int sockMerchant(int n, int ar_count, int* ar) {
+  int* map = calloc(ar_count, sizeof(int));
+
+  for (int i = 0; i < ar_count; i++) {
+    map[ar[i]]++;
+  }
+  int pairs = 0;
+
+  for (int i = 0; i < ar_count; i++) {
+    pairs++;
+  }
+}
