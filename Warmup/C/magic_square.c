4b1453eba1f837dcac596854683c7d23b46095f4
nirvanarsc <nirvana_rsc@yahoo.com>
2019-04-10 11:49:29 +0900

    Empty line

--- Warmup/C/magic_square.c
+++ Warmup/C/magic_square.c
@@ -6,7 +6,6 @@ int formingMagicSquare(int s_rows, int s_columns, int** s) {
       {8, 3, 4, 1, 5, 9, 6, 7, 2}, {6, 7, 2, 1, 5, 9, 8, 3, 4},
       {4, 3, 8, 9, 5, 1, 2, 7, 6}, {2, 7, 6, 9, 5, 1, 4, 3, 8},
       {4, 9, 2, 3, 5, 7, 8, 1, 6}, {2, 9, 4, 7, 5, 3, 6, 1, 8},
-
   };
 
   int* arr = flatten(s_rows, s_columns, s);
