39e46673df0584472f493de3c26f09b97e3e4b9c
Teo <nirvana_rsc@yahoo.com>
2019-04-23 18:20:33 +0900

    ACM Team in C (Complete)

--- Warmup/C/acm_team.c
+++ Warmup/C/acm_team.c
@@ -1,35 +1,46 @@
-#include <stdio.h>
+#include <string.h>
 #include "conveniences.h"
 
-void combinationUtil(int arr[], int data[], int start, int end, int index,
-                     int r) {
-  if (index == r) {
-    for (int j = 0; j < r; j++) printf("%d ", data[j]);
-    printf("\n");
-    return;
-  }
-
-  for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
-    data[index] = arr[i];
-    combinationUtil(arr, data, i + 1, end, index + 1, r);
-  }
-}
-
-void printCombination(int arr[], int n, int r) {
-  int data[r];
-
-  combinationUtil(arr, data, 0, n - 1, 0, r);
-}
-
 int* acmTeam(int topic_count, char** topic, int* result_count) {
+  *result_count = 2;
+  int* res = malloc(*result_count * sizeof(int));
+  int topic_size = strlen(topic[0]);
+  int max = INT_MIN;
+  int count = 0;
 
+  for (int i = 1; i <= topic_count; i++) {
+    for (int j = i + 1; j <= topic_count; j++) {
+      int total = 0;
+      for (int k = 0; k < topic_size; k++) {
+        if (topic[i - 1][k] == '1' || topic[j - 1][k] == '1') {
+          total++;
+        }
+      }
+      if (total > max) {
+        max = total;
+        count = 0;
+      }
+      if (total == max) {
+        count++;
+      }
+    }
+  }
 
-    
+  res[0] = max;
+  res[1] = count;
+  return res;
 }
 
 int main() {
-  int arr[] = {1, 2, 3, 4};
-  int r = 2;
-  int n = sizeof(arr) / sizeof(arr[0]);
-  printCombination(arr, n, r);
+  int topic_count = 4;
+  int result_count = 0;
+  char* topic[] = {"10101", "11100", "11010", "00101"};
+  int* res = acmTeam(topic_count, topic, &result_count);
+  simplePrintArray(res, result_count);
+
+  int topic_count2 = 6;
+  int result_count2 = 0;
+  char* topic2[] = {"11101", "10101", "11001", "10111", "10000", "01110"};
+  int* res2 = acmTeam(topic_count2, topic2, &result_count2);
+  simplePrintArray(res2, result_count2);
 }
